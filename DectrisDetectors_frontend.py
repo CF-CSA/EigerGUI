0bb43548b206d916e6271ed8ea99627ff357027d
Tim Gruene <tim.gruene@univie.ac.at>
2023-03-13 08:37:39 +0100

    GUI for Eiger2 R500 at Bruker D8 in Vienna

---
+++ DectrisDetectors_frontend.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 17:21:54 2022
+
+@author: quadro
+"""
+from requests import get
+from os.path import join, exists
+
+import multiprocessing as mp
+
+from DectrisDetectors_backend import DetectorBackend
+
+class DetectorFrontend:
+    "command line front end for convenient commands for Dectris Detectors"
+    def __init__(self, ip, vers="1.7.0", port = 80):
+        self.detector = DetectorBackend(ip, vers, port)
+        self.armID = -1
+        
+    """
+        Basic setup up. This will At startup, the detector has to be initialized first"
+    """
+    def setup(self, outdir, frame_time=1.0, elements=["Mo", "Cu"]):
+        #self.detector.send_command("initialize")
+        "Basic settings"
+        # we always use trigger mote 'ints'
+        self.detector.set_config("trigger_mode", "ints", "detector")
+        # save 10,000 frames per HDF5 file
+        self.detector.set_config("nimages_per_file", value=999, iface="filewriter")
+        # set to a very high number for continuous viewing
+        self.detector.set_config("nimages", 10000, iface="detector")
+        # default count and frame time: 100Hz
+        self.set_frame_time(frame_time)
+        # enable stream for STRELA viewer
+        self.detector.set_config("mode", "enabled", iface="stream")
+        # set list of allowed elements (for wavelength etc)
+        self.elements = elements
+        self.allowed_elements = self.detector.get_allowed("element", "detector")
+        # output directory
+        self.outdir = outdir
+
+    """
+    Get state for iface
+    """
+    def get_state(self, iface):
+        mystate = self.detector.get_status(iface=iface, param="state")
+        return mystate
+
+    """ 
+    return the list of files stored on DCU
+    """
+    def filelist(self):
+        flist = self.detector.get_status(iface='filewriter', param='data')
+        return flist
+
+    def detector_trigger(self):
+        self.detector.send_command("trigger")
+
+    """
+    Short cut for viewing w/o recording files:
+    - disarm detector (to ensure consistent state)
+    - disable filewriter 
+    - set nimages to very high number
+    - arm
+    - trigger as background process
+    """
+    def view(self):
+        "Disable writing and setup for viewing"
+        self.detector.send_command("disarm")
+        self.detector.set_config("mode", "disabled", "filewriter")
+        self.detector.set_config("nimages", 1000000, "detector")
+        self.arm()
+        triggerproc = mp.Process(target=self.detector_trigger)
+        triggerproc.start()
+    
+    """
+    start recording of data by enabling file writer. nimages must be set
+    """
+    def record(self):
+        "Disarm Detector, enable filewriter, arm, and triffer"
+        triggerproc = mp.Process(target=self.detector_trigger)
+        triggerproc.start()
+
+    """ 
+    wrapper for arming the detector which updates the sequence id as variable for 
+    self
+    """
+    def arm(self):
+        res = self.detector.send_command("arm")
+        self.armID = res["sequence id"]
+        
+    def stop(self):
+        "Short cut to disarm the detector"
+        self.detector.send_command("disarm")
+
+    def initialize(self):
+        "Initialize the detector. Resets ID"
+        self.detector.send_command("initialize")
+
+    def clear_files(self):
+        "Delete all files stored on DCU"
+        self.detector.send_command(command="clear", iface="filewriter")
+        
+    def set_frame_time(self, value):
+        "sets count_ and frame_time"
+        self.detector.set_config("count_time", value, "detector")
+        self.detector.set_config("frame_time", value, "detector")
+
+    def set_nimages(self, value):
+        "sets number of images to be recorded"
+        self.detector.set_config("nimages", value, "detector")
+
+    """
+    Shortcut for writer. Assumes that filepattern is set
+    """
+    def set_name_pattern(self, name_pattern):
+        "append timestamp and id to name_stem and set name_pattern"
+        self.detector.set_config("name_pattern", name_pattern, "filewriter")
+
+    """
+    wrapper to return name patterh
+    """
+    def get_name_pattern(self):
+        np = self.detector.get_config(param='name_pattern', iface='filewriter')
+        return np
+
+    """ 
+    set element type. Must conform to one of the elements supplied during setup
+    (default: Mo and Cu)
+    """
+    def set_element(self, element):
+        if (element in self.elements and element in self.allowed_elements):
+            self.detector.set_config("element", element, "detector")
+        else:
+            print(f"Element '{element}' not allowed")
+
+    """
+    record data for a certain time periond (seconds). Appends '_still' 
+    to current name pattern and resets it afterwards. Meant to record
+    images from crystals
+    """
+    def still(self, seconds=5):
+        "Record data for seconds seconds"
+        mystem = self.detector.get_config("name_pattern", "filewriter")
+        frate = self.detector.get_config("frame_time", "detector")
+
+        self.set_name_pattern(mystem+"still")
+
+        nimages = round(seconds/frate)
+
+        self.detector.send_command("disarm")
+        self.detector.set_config("mode", "enabled", "filewriter")
+        self.detector.set_config("nimages", nimages, "detector")
+        self.arm()
+        triggerproc = mp.Process(target=self.detector_trigger)
+        triggerproc.start()
+        triggerproc.join()
+        # revert original pattern
+        self.detector.set_config("name_pattern", mystem, "filewriter")
+        self.view()
+
+    """
+    download the given file from the DCU and save it to output directory
+    """
+    def save_file(self, filename, outdir):
+        url = f'http://{self.detector.ip_}:{self.detector.port_}/data/{filename}'
+        r = get(url)
+        localfile = join(outdir, filename)
+        if not exists (localfile):
+            open(localfile, 'wb').write(r.content)
+               # https://www.tutorialspoint.com/downloading-files-from-web-using-python
+
+    """
+    write out experimental parameters to a text file
+    """
+    def statusfile(self, filename):
+        print("statusfile is not yet implemented")
+
+    def wavelength(self):
+        w = self.detector.get_config(param="wavelength", iface="detector")
+        return w
+        
+            
+if __name__ == '__main__':
+    # univie = Quadro(SIP, SPORT, SVERS)
+    print ("error: DetectorFrontend should be importet and not run directly\n")
+    exit(1)
---
+++ EigerGUI.py
@@ -0,0 +1,487 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 13 22:46:21 2023
+
+@author: Tim Gruene
+"""
+import os.path
+
+from PyQt6 import (
+        QtWidgets,
+        QtGui,
+        QtCore
+        )
+
+import sys
+import time
+
+from DectrisDetectors_frontend import DetectorFrontend
+
+class EigerGUI(QtWidgets.QMainWindow):
+    "A simple GUI to run the Dectris SINGLA"
+    def __init__(self, ip="131.130.27.207"):
+        super(EigerGUI, self).__init__()
+
+        # list of parameters used for workflow
+        self.outdir = os.path.join('D:', os.sep, 'frames', 'D8', 'screening')
+        self.sampleID = "YourSampleID_no_Spaces"
+        self.xID = 0
+        self.armID = 0
+        self.name_pattern = "myfile"
+        self.twoTheta = -30.0
+        self.Omega = 60.0
+        self.Phi = 0.0
+        self.Chi = -35.0
+        self.D_mm = 34 #mm
+        self.Axis = "OMEGA"
+        self.source = "Mo"
+        # parameters with reasonable defaults
+        self.frame_time= 1.0 #seconds
+        #
+
+        self.setGeometry(50, 50, 600, 500)
+        self.setWindowTitle(f"EIGER2 R500 @ UniVie Bruker D8 {ip}")
+        self.show()
+
+        self.detector = DetectorFrontend(ip)
+        self.frame_time = 1.0
+        self.scan_range = 5.0
+        self.image_width = 0.5
+        self.detector.setup(frame_time=self.frame_time, outdir=self.outdir)
+
+        # generate the stem for output file and display filename
+        self.updatefilename()
+        self.setup()
+
+
+    def setup(self):
+        layout = QtWidgets.QVBoxLayout()
+
+        layout.addWidget(self.detector_info())
+
+        layout.addWidget(self.outputfiles())
+        layout.addWidget(self.screening())
+        layout.addWidget(self.exp_geometry())
+        layout.addWidget(self.recSettings())
+        layout.addWidget(self.control())
+
+        main = QtWidgets.QWidget()
+        main.setLayout(layout)
+        self.setCentralWidget(main)
+        
+
+    def detector_info(self):
+        "Show IP address of detector, status, etc"
+        my = QtWidgets.QGroupBox("Detector Info")
+
+        layout=QtWidgets.QHBoxLayout()
+
+        layout.addWidget(QtWidgets.QLabel(text="IP:"))
+        layout.addWidget(QtWidgets.QLabel(text=self.detector.detector.ip_))
+
+        layout.addWidget(QtWidgets.QLabel(text="Status:"))
+        self.det_state = QtWidgets.QLabel(text="idle") #self.detector.get_state('detector'))
+        layout.addWidget(self.det_state)
+        btn = QtWidgets.QPushButton(text="Update")
+        btn.clicked.connect(self.update_state)
+        layout.addWidget(btn)
+
+        btn = QtWidgets.QPushButton(text="Initialize")
+        btn.clicked.connect(lambda: self.detector.initialize())
+        layout.addWidget(btn)
+
+        vlayout=QtWidgets.QVBoxLayout()
+        vlayout.addLayout(layout)
+
+        layout=QtWidgets.QHBoxLayout()
+        layout.addWidget(QtWidgets.QLabel(text="Select source:"))
+        cb = QtWidgets.QComboBox()
+        cb.addItems(["Mo", "Cu"])
+        cb.currentIndexChanged.connect(self.new_source)
+        layout.addWidget(cb)
+
+        vlayout.addLayout(layout)
+
+        my.setLayout(vlayout)
+        return (my)
+
+    def outputfiles(self):
+        "Setup measurement"
+        my = QtWidgets.QGroupBox("Output data")
+        
+        layout = QtWidgets.QGridLayout()
+
+        layout.addWidget(QtWidgets.QLabel(text="Output dir:"), 0, 0)
+        self.le_outdir = QtWidgets.QLineEdit(text=self.outdir)
+        layout.addWidget(self.le_outdir, 0, 1)
+        pb = QtWidgets.QPushButton(text="Browse")
+        pb.clicked.connect(self.new_outdir)
+        layout.addWidget(pb, 0, 2)
+
+        layout.addWidget(QtWidgets.QLabel(text="Sample ID"), 1, 0)
+        le = QtWidgets.QLineEdit(maxLength=100)
+        le.setValidator(QtGui.QRegularExpressionValidator(QtCore.QRegularExpression("[A-Za-z0-9-_:]{100}")))
+        le.textChanged.connect(self.new_sampleId)
+        layout.addWidget(le, 1, 1)
+        
+        layout.addWidget(QtWidgets.QLabel("$id", self), 1, 2)
+        self.lbl_armID = QtWidgets.QLabel("0")
+        layout.addWidget(self.lbl_armID, 1, 3)
+        pb = QtWidgets.QPushButton("Update $id", self)
+        pb.clicked.connect(self.updateId)
+        layout.addWidget(pb, 1, 4)
+
+        pb= QtWidgets.QPushButton(text="list files")
+        pb.clicked.connect(self.file_list)
+        layout.addWidget(pb, 2, 0)
+
+        pb= QtWidgets.QPushButton(text="Download")
+        pb.clicked.connect(self.download)
+        layout.addWidget(pb, 2, 1)
+
+        pb= QtWidgets.QPushButton(text="Clear DCU files")
+        pb.clicked.connect(self.clearfiles)
+        layout.addWidget(pb, 2, 2)
+
+        my.setLayout(layout)
+        return (my)
+
+    """
+    Geometry settings, need to be copied from APEX3Server
+    """
+    def exp_geometry(self):
+        "Setup measurement"
+        my = QtWidgets.QGroupBox("Experimental Geometry (update manually from APEX Server")
+
+        layout = QtWidgets.QGridLayout()
+
+        layout.addWidget(QtWidgets.QLabel(text="Distance:"), 0, 0)
+        db = QtWidgets.QDoubleSpinBox(self, value=34, minimum=34, maximum=247)
+        db.setSuffix(" mm")
+        db.valueChanged.connect(self.new_distance)
+        layout.addWidget(db, 0, 1)
+
+        layout.addWidget(QtWidgets.QLabel(text="2Theta:"), 0, 2)
+        db = QtWidgets.QDoubleSpinBox(self, value=-15, minimum=-180, maximum=180)
+        db.setSuffix("°")
+        db.valueChanged.connect(self.new_twotheta)
+        layout.addWidget(db, 0, 3)
+
+        layout.addWidget(QtWidgets.QLabel(text="Omega"), 0, 4)
+        db = QtWidgets.QDoubleSpinBox(self, value=0, minimum=-180, maximum=180)
+        db.setSuffix("°")
+        db.valueChanged.connect(self.new_omega)
+        layout.addWidget(db, 0, 5)
+
+        layout.addWidget(QtWidgets.QLabel(text="Phi"))
+        db = QtWidgets.QDoubleSpinBox(self, value=0, minimum=-180.0, maximum=180.0)
+        db.setSuffix("°")
+        db.valueChanged.connect(self.new_phi)
+        layout.addWidget(db)
+
+        layout.addWidget(QtWidgets.QLabel(text="Chi"))
+        db = QtWidgets.QDoubleSpinBox(self, value=-25, minimum=-99.0, maximum=99.0)
+        db.setSuffix("°")
+        db.valueChanged.connect(self.new_chi)
+        layout.addWidget(db)
+
+        layout.addWidget(QtWidgets.QLabel(text="Rotation Axis:"), 2, 0)
+        rb = QtWidgets.QRadioButton(text="Omega")
+        rb.clicked.connect(lambda: self.new_axis("OMEGA"))
+        rb.setChecked(True)
+        layout.addWidget(rb, 2, 1)
+        rb = QtWidgets.QRadioButton(text="Phi")
+        rb.clicked.connect(lambda: self.new_axis("PHI"))
+        layout.addWidget(rb, 2, 2)
+        self.new_axis(self.Axis)
+
+        my.setLayout(layout)
+        return (my)
+
+    """ 
+    Settings for screening
+    """
+    def screening(self):
+        my = QtWidgets.QGroupBox("Screen Sample")
+        layout = QtWidgets.QGridLayout()
+
+        layout.addWidget(QtWidgets.QLabel(text="Scan Range:"), 0, 0)
+        sb = QtWidgets.QDoubleSpinBox(self, value=2.0, minimum=0.1, maximum=360 )
+        sb.valueChanged.connect(self.new_scan_range)
+        layout.addWidget(sb, 0, 1)
+
+        layout.addWidget(QtWidgets.QLabel(text="Image Width:"), 1, 0)
+        sb = QtWidgets.QDoubleSpinBox(self, value=0.5, minimum=0.1, maximum=2)
+        sb.valueChanged.connect(self.new_image_width)
+        layout.addWidget(sb, 1, 1)
+
+        layout.addWidget(QtWidgets.QLabel(text="Exposure time:"), 2, 0)
+        sb = QtWidgets.QDoubleSpinBox(self, value=1.0, minimum=0.25, maximum=360000)
+        sb.setSuffix(" s/image")
+        sb.valueChanged.connect(self.new_frame_time)
+        layout.addWidget(sb, 2, 1)
+
+        my.setLayout(layout)
+        return (my)
+
+    """
+    Settings used for recording data
+    """
+    def recSettings(self):
+        "Setup measurement"
+        my = QtWidgets.QGroupBox("Recording data")
+        
+        layout = QtWidgets.QGridLayout()
+        
+        layout.addWidget(QtWidgets.QLabel(text="nimages:"), 0,0)
+        sb = QtWidgets.QSpinBox(self, minimum=1, maximum=100000)
+        sb.setValue(1000)
+        sb.valueChanged.connect(self.new_nimages)
+        layout.addWidget(sb, 0, 1)
+        
+        layout.addWidget(QtWidgets.QLabel(text="frame_time:"), 1,0)
+        sb = QtWidgets.QDoubleSpinBox(self, minimum=0.01, maximum=5.0)
+        sb.setSuffix("s")
+        sb.setDecimals(2)
+        sb.setValue(self.frame_time)
+        sb.valueChanged.connect(self.new_frame_time)
+        layout.addWidget(sb, 1,1)
+        
+        layout.addWidget(QtWidgets.QLabel(text="δϕ/δt≈", alignment=QtCore.Qt.AlignmentFlag.AlignRight|QtCore.Qt.AlignmentFlag.AlignVCenter), 1, 2)
+        sb = QtWidgets.QDoubleSpinBox(self, minimum=-2.5, maximum=2.5)
+        sb.setSuffix("°/s")
+        sb.setDecimals(1)
+        sb.setValue(2.0)
+        sb.valueChanged.connect(self.new_phidot)
+        layout.addWidget(sb, 1, 3)
+
+        my.setLayout(layout)
+        return (my)
+    
+    def control(self):
+        "control buttons at bottom of GUI"
+        my = QtWidgets.QWidget()
+        layout = QtWidgets.QHBoxLayout()
+
+        btn = QtWidgets.QPushButton("Arm", self)
+        btn.clicked.connect(self.arm)
+        layout.addWidget(btn)
+
+        btn = QtWidgets.QPushButton("Record", self)
+        btn.clicked.connect(self.record)
+        layout.addWidget(btn)
+
+        btn = QtWidgets.QPushButton("View", self)
+        btn.clicked.connect(self.view)
+        layout.addWidget(btn)
+        
+        btn = QtWidgets.QPushButton("Stop", self)
+        btn.clicked.connect(self.stop)
+        layout.addWidget(btn)
+
+
+        btn = QtWidgets.QPushButton("Quit", self)
+        btn.clicked.connect(self.quitgui)
+        layout.addWidget(btn)
+        
+        my.setLayout(layout)
+        return (my)
+        
+    @QtCore.pyqtSlot()    
+    def arm(self):
+        "Prepares the recording of data"
+        s = self.update_state()
+        if s == "ready":
+            self.detector.stop()
+        self.updatefilename()
+        self.updateId()
+        self.detector.set_frame_time(self.frame_time)
+        self.detector.set_name_pattern(self.name_pattern)
+        n = int(self.scan_range / self.image_width)
+        self.detector.set_nimages(n)
+        self.detector.detector.set_config("mode", "enabled", "filewriter")
+        self.detector.arm()
+        self.update_state()
+        self.updateId()
+
+
+    @QtCore.pyqtSlot()
+    def record(self):
+        "Starts the recording of data"
+        s = self.update_state()
+        if s == "ready":
+            self.detector.record()
+        else:
+            print ("Error: detector needs to be armed first")
+        self.xdsparams()
+        self.update_state()
+
+    @QtCore.pyqtSlot()    
+    def view(self):
+        "Start viewing, without recording data"
+        self.updatefilename()
+        self.detector.view()
+        self.updateId()
+        print("Detector in view mode, not recording images")
+        
+    @QtCore.pyqtSlot()
+    def stop(self):
+        "Stops recording, stops rotations"
+        self.detector.stop()
+        self.updateId()
+        self.update_state()
+        print ("Detector disarmed")
+        
+    @QtCore.pyqtSlot()    
+    def quitgui(self):
+        "Stop the detector, then quit GUI"
+        self.detector.stop()
+        QtCore.QCoreApplication.instance().quit()
+
+    """
+    generates the filename for data recording. Contains the timestamp.
+    Therefore, this function should be called by the record button
+    before starting data recording
+    """
+    @QtCore.pyqtSlot()    
+    def updatefilename(self):
+        "generate the filename string displayed in GUI"
+        now = time.strftime("_%Y-%m-%d_%H%M%S")
+        self.name_pattern = self.sampleID+now+"_ID-$id"
+
+
+    """
+    The following functions receive data from the GUI elements
+    """
+    @QtCore.pyqtSlot()
+    def new_outdir(self):
+        dd = QtWidgets.QFileDialog.getExistingDirectory(directory=self.outdir,  caption="Choose Directory")
+        self.outdir = dd
+        self.le_outdir.setText(dd)
+
+    @QtCore.pyqtSlot(str)    
+    def new_sampleId(self, text):
+        self.sampleID = text
+        self.updatefilename()
+        
+    @QtCore.pyqtSlot(int)    
+    def new_xID(self, value):
+        self.xID = value
+        self.updatefilename()
+
+    QtCore.pyqtSlot(str)
+    def new_axis(self, value):
+        self.Axis = value
+        
+    @QtCore.pyqtSlot()
+    def updateId(self):
+        "retrieve the current ID from Detector"
+        self.armID = self.detector.armID
+        self.lbl_armID.setText(str(self.armID))
+        self.updatefilename()
+
+    @QtCore.pyqtSlot(int)
+    def new_nimages(self, value):
+        self.nimages = value
+        
+    @QtCore.pyqtSlot("double")
+    def new_frame_time(self, value):
+        self.frame_time = value
+
+    @QtCore.pyqtSlot("double")
+    def new_scan_range(self, value):
+        self.scan_range = value
+
+    @QtCore.pyqtSlot("double")
+    def new_image_width(self, value):
+        self.image_width = value
+
+    @QtCore.pyqtSlot("double")
+    def new_phidot(self, rate=2.0):
+        self.phidot = rate
+        print("New rate: {}".format(self.phidot))
+        
+    @QtCore.pyqtSlot("double")
+    def new_distance(self, value):
+        self.D_mm = value
+        
+    @QtCore.pyqtSlot("double")
+    def new_twotheta(self, value):
+        self.twoTheta = value
+
+    @QtCore.pyqtSlot("double")
+    def new_omega(self, value):
+        self.Omega = value
+
+    @QtCore.pyqtSlot("double")
+    def new_phi(self, value):
+        self.Phi = value
+
+    @QtCore.pyqtSlot("double")
+    def new_chi(self, value):
+        self.Chi = value
+
+    @QtCore.pyqtSlot()
+    def update_state(self):
+        res = self.detector.get_state("detector")
+        self.det_state.setText(res)
+        return res
+
+    @QtCore.pyqtSlot("int")
+    def new_source(self, idx):
+        sources = ["Mo", "Cu"]
+        self.source = sources[idx]
+        self.detector.set_element(self.source)
+
+    @QtCore.pyqtSlot()
+    def file_list(self):
+        flist = self.detector.filelist()
+        print (flist)
+
+    @QtCore.pyqtSlot()
+    def download(self):
+        flist = self.detector.filelist()
+        for f in flist:
+            self.detector.save_file(f, self.outdir)
+
+    @QtCore.pyqtSlot()
+    def clearfiles(self):
+        print ("Deleting files on DCU")
+        self.detector.clear_files()
+
+    def xdsparams(self):
+        # make sure ID is up to date
+        self.updateId()
+        fname = f"PARAMS_{self.sampleID}_ID{str(self.armID)}.INP"
+        fname = os.path.join(self.outdir, fname)
+        nimg = int(self.scan_range / self.image_width)
+        if self.source == "Cu":
+            wavelength = 1.54184 # from APEX server listing
+        else:
+            wavelength = 0.70931
+
+        name_pattern = self.detector.get_name_pattern()
+        name_pattern = name_pattern.replace('$id', str(self.armID))
+
+        with open(fname, 'w') as f:
+            f.write(f"DETECTOR_DISTANCE= {self.D_mm}\n")
+            f.write(f"2THETA= {self.twoTheta}\n")
+            f.write(f"OMEGA= {self.Omega}\n")
+            f.write(f"PHI= {self.Phi}\n")
+            f.write(f"CHI= {self.Chi}\n")
+            f.write(f"AXIS= {self.Axis}\n")
+            f.write(f"NIMAGES= {nimg}\n")
+            f.write(f"SWEEP= {str(self.scan_range)}\n")
+            f.write(f"DATA_RANGE= 2 {str(nimg-1)}\n")
+            f.write(f"X-RAY_WAVELENGTH= {wavelength:.5f}\n")
+            f.write(f"NAME_TEMPLATE_OF_DATA_FRAMES= {name_pattern}_??????.h5\n")
+        print (f"Parameters for sfrmtools written to {fname}\n")
+
+
+if __name__ == "__main__":
+    print("Starting main")
+    app = QtWidgets.QApplication(sys.argv)
+    eigerGui = EigerGUI()
+    app.exec()
+        
