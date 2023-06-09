216efa1decd3f715280e0476abf134bedd581f61
Tim Gruene <tim.gruene@univie.ac.at>
2023-06-09 22:50:08 +0200

    Reading of Bruker .exp-file accomplished; reading of parameters and conversion to XDS.INP parameters is 'fleissarbeit'

--- BrukerExpFile.py
+++ BrukerExpFile.py
@@ -1,9 +1,10 @@
 import json
 
-class Exp:
+class ExpFile:
     "Read and process experimental description file from Bruker"
-    def __init(self, filename):
+    def __init__(self, filename):
         self.filename = filename
+        self.runs = []
 
     def readexp(self):
         with open(self.filename, 'r') as expfile:
@@ -21,13 +22,25 @@ class Exp:
     extract information from runs
     """
     def getinfo(self):
-        wl = json.loads(json.dumps(self.exp['scanset'][0]))
+        wl = json.loads(json.dumps(self.exp[0]))
+        " debugging: print wavelength"
         self.wavelength = wl['wavelength']
-        self.json_runs = json.loads(json.dumps(self.exp['scanset'][1:-1]))
+        print (f"Lambda = {self.wavelength}")
+        self.json_runs = json.loads(json.dumps(self.exp[1:-1]))
+        """
+        access information in self.json_runs with e.g.
+        run = self.json_runs[0]['p']['chi']
+        or 
+        axis = self.json_runs[0]['angle']
+        keys: attenutation, sensitivity, frametime, readout,step, active, end, angle
+        keys in p: phi, type (?),dx (Delta), chi,theta, omega
+        """
+        for run in self.json_runs:
+            if run['active']:
+                "create a list of active runs"
+                self.runs.append(run)
+        print(f"Number of active runs: {len(self.runs)}")
         # a run as a subkeyword ['active'] which is 1 or 0
         # looping: for j in (self.json_runs):
         #  if j['active'] -> paramters in j['p']
         #  run.params = json.loads(json.dumps(j['p'])
-
-    def num_runs(self):
-        self.runs =
