7c482cfa37dbc2f83ca57bd31576887c927edcea
Tim Gruene <tim.gruene@univie.ac.at>
2023-06-14 14:13:57 +0200

    XDSparams.py updates template from bruker parameters

--- XDSparams.py
+++ XDSparams.py
@@ -1,29 +1,54 @@
 import os
 
+import numpy as np
+
 """
 Sets up XDS parameters from experimental parameters. Derived from SFRMtools. Also creates
 subdirectories with XDS.INP
 """
 class XDSparams:
-    def __init__(self, xdstemplate):
+    def __init__(self, xdstemplate, name_template, data_range):
         self.xdstempl= xdstemplate
-        self.wavelength = 1.541840 # default: CuKa
-        self.rotation_axis = [ 0, -1, 0] # default :positive rotation about omega
+        self.param_list = [("NAME_TEMPLATE_DATA_FRAMES=", f"{name_template}"), ("DATA_RANGE=", f"{data_range}")]
+        self.done_params = []
 
     """
-    replace parameters in xdstemplate with given ones
+    Experimental settings based on oscilation width (float), wavelength (float), axis (string), omega (radians)
+    chi (float, radians), rotdir (+/-1), start (float, radians
     """
-    def update(self, param_list):
+    def settings(self, oscw, wavelength, theta, axis, omega, chi, rotdir, start):
+        my = ("OSCILLATION_RANGE=", f"{oscw:6.3f}")
+        self.param_list.append(my)
+        my = ("X-RAY_WAVELENGTH=", f"{wavelength:8.6f}")
+        self.param_list.append(my)
+        my = ("STARTING_ANGLE=", f"{180./np.pi*start:6.4f}")
+        self.param_list.append(my)
+        self.detector_x_axis(theta)
+        self.rotation_axis(axis, omega, chi, rotdir)
+        self.update()
+    """
+    replace parameters in xdstemplate with given ones
+    This function should be last before writing XDS.INP 
+    and param_list needs to be populated with keywords and values
+    """
+    def update(self):
         self.xdsinp = [] # empty XDS.INP
         with open (self.xdstemplate, 'r') as f:
             for line in f:
                 [keyw, rem] = self.uncomment(line)
-                for p in param_list:
+                for p in self.param_list:
                     key = p[0]
                     val = p[1]
                     keyw = self.replace(keyw, key, value)
                     self.xdsinp.append(' '+keyw+' ' +rem)
+        leftovers = set(self.param_list) - set(self.done_params)
+        for l in leftovers:
+            self.xdsinp.append(f" {l[0]} {l[1]}")
 
+    """
+    search for exclamation marks and split the line into keyw (and value) 
+    as well as remark. Copied from dectris2xds.py
+    """
     def uncomment(self, line):
         "find exclamation mark and separate string at this point"
         if ("!" in line):
@@ -37,13 +62,44 @@ class XDSparams:
 
     """
     checks whether keyw is present in line (including '=' and
-    replaces the subsequent value with val"""
+    replaces the subsequent value with val.
+    Copied from dectris2xds.py
+    """
     def replace(self, line, keyw, val):
         if (keyw in line):
             line = ' '+keyw+' ' +str(val)
+            # keep track of replaced parameters
+            self.done_params.append([(keyw, val)])
         else:
             line = line
         return line
+
+    """
+    Calculate the rotation axis from the input angles
+    direction should be a number, positive or negative
+    omega and chi are angles in radians
+    """
+    def rotation_axis(self, axis, omega, chi, direction):
+        direction = np.sign(direction)
+        if axis == 'omega':
+            rotaxis = direction*[0, -1, 0]
+        elif axis == 'phi':
+            x = direction*np.cos(omega)*np.sign(chi)
+            y = direction*np.cos(chi)
+            z = direction*np.sin(omega)*np.sin(chi)
+            rotaxis = [x, y, z]
+        else:
+            raise ValueError("Rotation axes other than omega or phi invalid")
+        my = ("ROTATION_AXIS=", f" {rotaxis[0]:6.5f} {rotaxis[1]:6.5f} {rotaxis[2]:6.5f}")
+        self.param_list.append(my)
+
+    """ 
+    calculate and set the detector X-axis from theta
+    """
+    def detector_x_axis(self, theta):
+        detx = [np.cos(2*theta), 0.0, np.sin(2*theta)]
+        my  = ("DIRECTION_OF_DETECTOR_X-AXIS=", f"{detx[0]:9.6f} {detx[1]} {detx[2]:9.6f}")
+        self.param_list.append(my)
 
     """
     writes content of self.xdsinp into outdir as XDS.INP"""
