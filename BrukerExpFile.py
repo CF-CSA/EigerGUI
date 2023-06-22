fixed a bug in runtime calculation and update sweep from EXP file

BrukerExpFile.py
@@ -77,12 +77,13 @@ class ExpFile:
             myrun["theta"] = params["theta"]
             myrun["omega"] = params["omega"]
             if "frametime" in myrun and "frameangle" in myrun:
-                myrun['runtime'] = myrun["frametime"] * (myrun["end"] - myrun["start"])/myrun["frameangle"]
+                myrun['runtime'] = myrun["frametime"] * (np.abs(myrun["end"] - myrun["start"]))/myrun["frameangle"]
                 print(f"frametime = {myrun['frametime']}s")
             else:
-                print("Cannot calculate runtime take from GUI")
+                print("Cannot calculate runtime,  take from GUI")
             self.runs.append(myrun)
         print(f"Number of active runs: {len(self.runs)}")
+
 
         # a run as a subkeyword ['active'] which is 1 or 0
         # looping: for j in (self.json_runs):

EigerGUI.py
@@ -234,17 +234,20 @@ class EigerGUI(QtWidgets.QMainWindow):
         name_template = name_template.replace("$id", str(self.armID))
         print(f"name template with ID reads {name_template}")
         name_template = name_template + "_??????.h5"
+
         print(f"name template with suffix reads {name_template}")
         for idx, run in enumerate(self.experiment.runs):
             rundir = self.workdir+os.path.sep+f"run{idx+1:02d}"
             data_range = f"{idx*self.nimages} {(idx+1)*self.nimages}"
             xds=XDSparams(name_template, data_range)
+            sweep = run['end'] - run['start']
+            self.new_scan_range(sweep)
             if "runtime" in run:
                 "Check consistency between EXP-file and GUI"
                 rt_Dectris = self.frame_time*self.nimages
                 if abs(rt_Dectris - run["runtime"]) > 0.001:
                     raise ValueError("Inconsistency between EXP and GUI frame time")
-            dir = np.sign(run['end'] - run['start'])
+            dir = np.sign(sweep)
             xds.settings(self.image_width, self.experiment.wavelength, run['theta'], run['angle'], run['omega'], run['chi'], dir, run['start'])
             xds.update(self.xdstemplate)
             xds.xdswrite(rundir)