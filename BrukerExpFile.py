990fea281ffe178fc6c367d58e2f79a542e2c8e1
Tim Gruene <tim.gruene@univie.ac.at>
2023-11-21 12:21:42 +0100

    added a new exp file for testing; cleaned up printing for ExpFile, and added verbosity level

--- BrukerExpFile.py
+++ BrukerExpFile.py
@@ -4,10 +4,39 @@ import numpy as np
 class ExpFile:
     "Read and process experimental description file from Bruker"
 
-    def __init__(self, filename):
+    def __init__(self, filename, verbosity=0):
         self.filename = filename
+        self.allruns = []
         self.runs = []
         self.total_images = 0
+        self.verbosity = verbosity
+        """
+        EXP file contains a comment block, followed by a 'dict' with the key 'scanset'
+        content of scanset is a list of the individual runs:
+        Runs are dictioneries with the entries:
+        attenuation - number, expect '1'
+        sensitivity - int 3 (?)
+        frametime   - tuple, e.g. ('replace', 120.0)
+        readout     - int, 3: shutterless
+        step        - float, rad
+        active      - Bool True/False
+        end         - float, rad; end - start = SWEEP
+        angle       - word, rotation axis
+        p           - dict with parameters
+        detectorsizemargin - float
+        start       - float, radians; end - start = SWEEP
+        frameangle  - tuple, e.g. ('replace', 1.0471975511965976) = WIDTH (per frame)
+        anglemargin - float, radians?
+        invertscan -  direction, True/False
+        content of p (all floats except type):
+          phi
+          type 
+          dx [mm]
+          chi
+          theta
+          omega
+        """
+
 
     """
     wrapper that calls all necessary functions to extract information from EXP-file
@@ -27,39 +56,38 @@ class ExpFile:
             expf = f.read()
 
         data = ast.literal_eval(expf)
-        self.exp = data["scanset"]
+        self.allruns = data["scanset"] # runs is a list; each list entry is a run, including one for setting wavelength
+        if self.verbosity > 1:
+            print(f"---> Reading {len(self.runs)} run entries from file {self.filename}")
 
     """
     extract information from runs
     """
 
     def getinfo(self):
-        self.wavelength = self.exp[0]["wavelength"]
-        print(f"Lambda = {self.wavelength}")
-        allruns = self.exp[1:]
         """
-        access information in self.json_runs with e.g.
-        run = self.json_runs[0]['p']['chi']
-        or 
-        axis = self.json_runs[0]['angle']
-        keys: attenutation, sensitivity, frametime, readout,step, active, end, angle
-        keys in p: phi, type (?),dx (Delta), chi,theta, omega
+        find active runs, find wavelength setting
+        and for each experimental run, extract the geometry settings.
         """
         active_runs = []
-        for run in allruns:
-            if run["active"] and "p" in run:
+        run: dict
+        for run in self.allruns:
+            if run["active"] is True and "wavelength" in run:
+                self.wavelength = run["wavelength"]
+                if self.verbosity > 0:
+                    print(f"---> Lambda = {self.wavelength}")
+            if run["active"] is True and "p" in run:
                 "create a list of active runs"
                 active_runs.append(run)
             else:
-                print(f"Inactive run: {run}")
-        print(f"Number of active runs: {len(active_runs)}")
+                if self.verbosity > 1:
+                    print(f"---> Inactive or setting run: {run}")
+        if self.verbosity > 0:
+            print(f"---> Number of active runs: {len(active_runs)}")
         """convert run structure to something reasonable for easier processing"""
         for run in active_runs:
             """start angle in parameters.end, end angles in end outside p"""
-            if "p" not in run:
-                """This is not a run, but some other description"""
-                print(f"Error, no parameters in run {run}")
-            else:
+            if self.verbosity > 1:
                 print(f"{run['p']}")
             params = run["p"]
             myrun = {}
@@ -81,20 +109,18 @@ class ExpFile:
                 myrun["nimages"] = abs(myrun["end"] - myrun["start"]) / myrun["frameangle"]
                 myrun["nimages"] = round(myrun["nimages"])
                 self.total_images = self.total_images + myrun["nimages"]
-                print(f"rounded number of images: {myrun['nimages']}")
+                if self.verbosity > 1:
+                    print(f"---> rounded number of images: {myrun['nimages']}")
             if "frametime" in myrun and "frameangle" in myrun:
                 myrun["runtime"] = (
                     myrun["frametime"]
                     * (abs(myrun["end"] - myrun["start"]))
                     / myrun["frameangle"]
                 )
-                print(f"frametime = {myrun['frametime']}s, runtime = {myrun['runtime']}")
+                if self.verbosity > 1:
+                    print(f"---> frametime = {myrun['frametime']}s, runtime = {round(myrun['runtime'], ndigits=2)}")
             else:
-                print("Cannot calculate runtime,  take from GUI")
+                print("### Cannot calculate runtime,  take from GUI")
             self.runs.append(myrun)
-        print(f"Number of active runs: {len(self.runs)}")
-
-        # a run as a subkeyword ['active'] which is 1 or 0
-        # looping: for j in (self.json_runs):
-        #  if j['active'] -> paramters in j['p']
-        #  run.params = json.loads(json.dumps(j['p'])
+        if self.verbosity > 0:
+            print(f"Number of active runs: {len(self.runs)}")
